d0c739574b7d7ccb23192435d10ee9b952cc300f
Dan <137257813+ZNackly@users.noreply.github.com>
2024-01-04 15:22:06 +0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Hello ");
+        builder.append(15);
+        System.out.println(builder);
+    }
+}
